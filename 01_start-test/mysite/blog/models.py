054d29fe138ddaf743eafd8c7f83b4792805e0de
ibbentu <240755087+ibbentu@users.noreply.github.com>
2026-04-07 01:58:27 +0900

    Auto commit

--- 01_start-test/mysite/blog/models.py
+++ 01_start-test/mysite/blog/models.py
@@ -2,15 +2,33 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+    # status
+    class Status(models.Textchoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+
+    # model
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
+
+    # datetime
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ('-publish')
-        
+        # order
+        ordering = ['-publish']
+
+        # index
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
     def __str__(self):
         return self.title
