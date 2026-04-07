7d156e1b3843458b56fee8d12baaf5f0ebd8c456
ibbentu <240755087+ibbentu@users.noreply.github.com>
2026-04-08 03:45:30 +0900

    Auto commit

--- 01_start-test/mysite/blog/models.py
+++ 01_start-test/mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     # status
@@ -16,6 +17,12 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
 
     # datetime
     publish = models.DateTimeField(default=timezone.now)
